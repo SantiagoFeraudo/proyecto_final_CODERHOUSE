e86c9f5f4d8974346a848a36cfcdfe6dc0c8c846
SantiagoFeraudo <santiagoferaudo5@gmail.com>
2023-08-16 20:31:34 -0300

    cambiando gitignore

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import render_posts, post_detail
 from blog import views
 
-from django.conf.urls.static import static
-from django.conf import settings
-
 app_name = 'blog'
 
 urlpatterns = [
@@ -12,7 +9,3 @@ urlpatterns = [
     path("<int:post_id>", post_detail, name="post_detail"),
     path("<int:post_id>", views.PostView.as_view(), name="post_detail"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                         document_root=settings.MEDIA_ROOT)
